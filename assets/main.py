22fe65d98df818f971b6aff69c1aad2f05b4c335
ustundag <ustundaganil@gmail.com>
2019-05-31 18:29:26 +0200

    opencv mouse_click event added.

--- assets/main.py
+++ assets/main.py
@@ -9,54 +9,62 @@ which is larger than the len( labels ).
 
 You can use the data/rgb images as input for the network.
 You can get the correct label by converting the corresponding image from data/semantic
-using utils.py to get the correct index from the color coding.
+susing utils.py to get the correct index from the color coding.
 """
 
 import os
 from utils import *
+import random
 
-#PATH_Base = os.path.dirname(os.path.dirname((os.path.realpath(__file__)))) # for just atom.io
-PATH_Base = os.path.dirname(os.getcwd()) # for pycharm general
-PATH_RGB_Images = os.path.join(PATH_Base, "area_3", "data", "rgb")
-PATH_Semantic_Images = os.path.join(PATH_Base, "area_3", "data", "semantic")
+PATH_Base = os.path.dirname(os.path.dirname((os.path.realpath(__file__)))) # for just atom.io
+#PATH_Base = os.path.dirname(os.getcwd()) # for pycharm general
+PATH_RGB_Images           = os.path.join(PATH_Base, "area_3", "data", "rgb")
+PATH_Semantic_Images      = os.path.join(PATH_Base, "area_3", "data", "semantic")
 PATH_Semantic_Labels_Json = os.path.join(PATH_Base, "assets", "semantic_labels.json")
 
-def main():
-    labels = load_labels(PATH_Semantic_Labels_Json)
-    LIST_Semantic_Images = os.listdir(PATH_Semantic_Images)
-    # LIST_Semantic_Images[0] -> .gitkeep is the first element
-    #PATH_im = os.path.join(PATH_Semantic_Images, LIST_Semantic_Images[10])
-    PATH_im = os.path.join(PATH_Semantic_Images, "camera_bbda26272dd346e595ed8e87a2d91020_office_3_frame_18_domain_semantic.png")
+labels = load_labels(PATH_Semantic_Labels_Json)
+LIST_Semantic_Images = os.listdir(PATH_Semantic_Images)
+# LIST_Semantic_Images[0] -> .gitkeep is the first element
 
-    image = cv2.imread(PATH_im)
-    x_coor = 900
-    y_coor = 100
-    # OpenCV returns pixels in order of BGR. So, Flip it to be RGB.
-    pix = np.flip(image[x_coor, y_coor])
-    i = get_index(pix)
-    instance_label = labels[i]
-    instance_label_as_dict = parse_label(instance_label)
-    print(instance_label_as_dict)
+def mouse_clicked(event, x, y, flags, images):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        write_object_name(images, x, y)
 
-    height, width, depth = image.shape
-    image_scaled = cv2.resize(image, (int(height/2), int(width/2)))
-
+def write_object_name(images, x, y):
+    cv2.circle(images[1], (x, y), 5, (255, 255, 255), -1)
     font      = cv2.FONT_HERSHEY_SIMPLEX
-    position  = (int(y_coor/2)+10, int(x_coor/2)-10)
+    position  = (x-25, y+25)
     fontScale = 1
     fontColor = (255,255,255)
     lineType  = 2
-    text      = instance_label_as_dict["instance_class"]
-    cv2.putText(image_scaled, text,
-        position,
-        font,
-        fontScale,
-        fontColor,
-        lineType)
-    cv2.imshow('image', cv2.circle(image_scaled, (int(y_coor/2), int(x_coor/2)), 10, (255, 255, 255), -1))
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    text      = get_object_name(images[0], x*2, y*2)
+    cv2.putText(images[1], text, position, font, fontScale, fontColor, lineType)
 
+def get_object_name(image, x, y):
+    # OpenCV returns pixels in order of BGR. So, Flip it to be RGB.
+    pix = np.flip(image[y, x])
+    i = get_index(pix)
+    instance_label = labels[i]
+    instance_label_as_dict = parse_label(instance_label)
+    return instance_label_as_dict["instance_class"]
+
+def main():
+    random_i = random.randint(1, len(LIST_Semantic_Images)-1)
+    PATH_im = os.path.join(PATH_Semantic_Images, LIST_Semantic_Images[random_i])
+    image = cv2.imread(PATH_im)
+    height, width, depth = image.shape
+    image_scaled = cv2.resize(image, (int(height/2), int(width/2)))
+
+    cv2.namedWindow('image')
+    cv2.setMouseCallback('image', mouse_clicked, [image, image_scaled])
+
+    while True:
+        cv2.imshow('image', image_scaled)
+        # Esc key to terminate
+        if cv2.waitKey(20) == 27:
+            break
+
+    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
