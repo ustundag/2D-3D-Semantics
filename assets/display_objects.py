2680366c7974c52ef4acddc2f2671ea3a786e600
ustundag <ustundaganil@gmail.com>
2019-08-27 22:55:25 +0200

    spherical experiments and some clean-up.

--- assets/display_objects.py
+++ assets/display_objects.py
@@ -1,30 +1,65 @@
-from utils import *
+"""
+Semantic images come in two variants, semantic and semantic_pretty.
+They both include information from the point cloud annotations,
+but only the semantic version should be used for learning.
+The semantic images have RGB images which are direct 24-bit base-256 integers
+which contain an index into /assets/semantic_labels.json.
+Pixels where the data is missing are encoded with the color #0D0D0D
+which is larger than the len( labels ).
 
-scale_size = 1
-labels = load_labels('C:/Users/ustundag/GitHub/2D-3D-Semantics/assets/semantic_labels.json')
+You can use the data/rgb images as input for the network.
+You can get the correct label by converting the corresponding image from data/semantic
+susing utils.py to get the correct index from the color coding.
+"""
+
+import os
+from utils import *
+import random
+
+PATH_Base = os.path.dirname(os.path.dirname((os.path.realpath(__file__)))) # for just atom.io
+#PATH_Base = os.path.dirname(os.getcwd()) # for pycharm general
+PATH_RGB_Images           = os.path.join(PATH_Base, "area_3", "data", "rgb")
+PATH_Semantic_Images      = os.path.join(PATH_Base, "area_3", "data", "semantic")
+PATH_Semantic_Labels_Json = os.path.join(PATH_Base, "assets", "semantic_labels.json")
+
+labels = load_labels(PATH_Semantic_Labels_Json)
+LIST_Semantic_Images = os.listdir(PATH_Semantic_Images)
+# LIST_Semantic_Images[0] -> .gitkeep is the first element
 
 def mouse_clicked(event, x, y, flags, images):
     if event == cv2.EVENT_LBUTTONDBLCLK:
-        object = get_object_name(images[0], x*scale_size, y*scale_size)
-        print(object)
+        write_object_name(images, x, y)
+
+def write_object_name(images, x, y):
+    cv2.circle(images[1], (x, y), 3, (255, 255, 255), -1)
+    font      = cv2.FONT_HERSHEY_SIMPLEX
+    position  = (x-20, y+20)
+    fontScale = 0.5
+    fontColor = (255,255,255)
+    lineType  = 1
+    text      = get_object_name(images[0], x*2, y*2)
+    cv2.putText(images[1], text, position, font, fontScale, fontColor, lineType)
 
 def get_object_name(image, x, y):
     # OpenCV returns pixels in order of BGR. So, Flip it to be RGB.
+    object = 'MISSING'
     pix = np.flip(image[y, x])
-    print('pixels: ' + str(pix))
     i = get_index(pix)
     if i < len(labels):
         instance_label = labels[i]
         instance_label_as_dict = parse_label(instance_label)
-        return instance_label_as_dict["instance_class"]
-    return '<MISSING>'
-
-image = cv2.imread('camera_81a9927c6b224f4bb5e6b9fbdcfae9c0_office_3_frame_25_domain_semantic_scaled.png')
-#image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-height, width, depth = image.shape
-image_scaled = cv2.resize(image, (int(height/scale_size), int(width/scale_size)))
+        object = instance_label_as_dict["instance_class"]
+    print('pixels: ' + str(pix) + ' = ' + object)
+    return object
 
 def main():
+    random_i = random.randint(1, len(LIST_Semantic_Images)-1)
+    PATH_im = os.path.join(PATH_Semantic_Images, LIST_Semantic_Images[random_i])
+    #image = cv2.imread(PATH_im)
+    image = cv2.imread('camera_0ccf3c78ef354902b516c62ef8fb7cf1_lounge_2_frame_13_domain_semantic.png')
+    height, width, depth = image.shape
+    image_scaled = cv2.resize(image, (int(height/2), int(width/2)))
+
     cv2.namedWindow('image')
     cv2.setMouseCallback('image', mouse_clicked, [image, image_scaled])
 
